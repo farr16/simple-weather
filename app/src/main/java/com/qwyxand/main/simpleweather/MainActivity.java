87834c6a7634b3e70dfbf23f77035c2c9a765e91
Matthew Farr <msamfarr@outlook.com>
2016-08-18 12:19:22 -0700

    Adds a new class which stores information about the weather grabbed from the api call's result.

--- app/src/main/java/com/qwyxand/main/simpleweather/MainActivity.java
+++ app/src/main/java/com/qwyxand/main/simpleweather/MainActivity.java
@@ -2,12 +2,47 @@ package com.qwyxand.main.simpleweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView weather_description;
+    private TextView current_temperature;
+    private TextView daily_low;
+    private TextView daily_high;
+    private TextView humidity;
+    private TextView wind_speed;
+    private Switch temp_toggle;
+
+    private String base_url = "http://api.openweathermap.org/data/2.5/weather?id=";
+    private String suffix_url = "&APPID=";
+    // This will be replaced with a dynamic means of setting location, but will be used for testing
+    private String city_id = "5751632";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //BuildConfig.OPEN_WEATHER_API_KEY
+
+        weather_description = (TextView) findViewById(R.id.weather_description_display);
+        current_temperature = (TextView) findViewById(R.id.current_temp_display);
+        daily_low = (TextView) findViewById(R.id.daily_low_display);
+        daily_high = (TextView) findViewById(R.id.daily_high_display);
+        humidity = (TextView) findViewById(R.id.humidity_display);
+        wind_speed = (TextView) findViewById(R.id.humidity_display);
+        temp_toggle = (Switch) findViewById(R.id.temp_format_switch);
+
+        //test code for making sure the url is created properly
+        String api_call_url = base_url + city_id + suffix_url + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+
+        weather_description.setText(api_call_url);
+
+    }
+
+    public void setWeatherGUI(Weather w) {
+        return;
     }
 }
---
+++ app/src/main/java/com/qwyxand/main/simpleweather/Weather.java
@@ -0,0 +1,55 @@
+package com.qwyxand.main.simpleweather;
+
+/**
+ * Created by Matthew on 8/17/2016.
+ *
+ * Defines a class called Weather.
+ *
+ * Weather objects are used to store the data displayed on the front page of the app.
+ * The data fields included in a weather object are  weather description, temperature, daily highs
+ * and lows, humidity, and wind speed.
+ */
+public class Weather {
+    private String description;
+    private double temp_K;
+    private double high_K;
+    private double low_K;
+    private double humidity;
+    private double wind_speed;
+
+    /* Constructor for the Weather class.
+    * Initializes a weather object with empty or zero values for all attributes*/
+    public Weather() {
+        description = "";
+        temp_K = 0.0;
+        high_K = 0.0;
+        low_K = 0.0;
+        humidity = 0.0;
+        wind_speed = 0.0;
+    }
+
+    public void setDescription(String desc) {description = desc;}
+
+    public void setTemp_K(double temp) {temp_K = temp;}
+
+    public void setHigh_K(double high) {high_K = high;}
+
+    public void setLow_K(double low){low_K = low;}
+
+    public void setHumidity(double humid) {humidity = humid;}
+
+    public void setWind_speed(double wind) {wind_speed = wind;}
+
+    public String getDescription() {return description;}
+
+    public double getTemp_K() {return temp_K;}
+
+    public double getHigh_K() {return high_K;}
+
+    public double getLow_K() {return low_K;}
+
+    public double getHumidity() {return humidity;}
+
+    public double getWind_speed() {return wind_speed;}
+
+}
