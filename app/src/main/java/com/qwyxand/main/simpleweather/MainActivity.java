62fa6a7a19a296474fa5163f89fedf62ea9af0dd
Matthew Farr <msamfarr@outlook.com>
2016-08-18 21:34:00 -0700

    Cleans up comments, moves class variables that are only used in one method to local variables of that method, removes unnecessary return statements on void methods.

--- app/src/main/java/com/qwyxand/main/simpleweather/MainActivity.java
+++ app/src/main/java/com/qwyxand/main/simpleweather/MainActivity.java
@@ -15,17 +15,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView wind_speed;
     private Switch temp_toggle;
 
-    private String base_url = "http://api.openweathermap.org/data/2.5/weather?id=";
-    private String suffix_url = "&APPID=";
-    // This will be replaced with a dynamic means of setting location, but will be used for testing
-    private String city_id = "5751632";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //BuildConfig.OPEN_WEATHER_API_KEY
 
         weather_description = (TextView) findViewById(R.id.weather_description_display);
         current_temperature = (TextView) findViewById(R.id.current_temp_display);
@@ -34,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         humidity = (TextView) findViewById(R.id.humidity_display);
         wind_speed = (TextView) findViewById(R.id.wind_speed_display);
         temp_toggle = (Switch) findViewById(R.id.temp_format_switch);
+
+        String base_url = "http://api.openweathermap.org/data/2.5/weather?id=";
+        String suffix_url = "&APPID=";
+        //this will be replaced with a dynamic means of setting location, but will be used for tests
+        String city_id = "5751632";
 
         //test code for making sure the url is created properly
         String api_call_url = base_url + city_id + suffix_url + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
@@ -44,15 +42,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void setWeatherGUI(Weather w) {
         weather_description.setText(w.getDescription());
-        current_temperature.setText("" + w.getTemp_K());
-        daily_low.setText("" + w.getLow_K());
-        daily_high.setText("" + w.getHigh_K());
-        humidity.setText("Humidity: " + w.getHumidity() + "%");
-        wind_speed.setText("Wind Speed: " + w.getWind_Speed());
+        String curr_temp_text = "" + w.getTemp_K();
+        current_temperature.setText(curr_temp_text);
+        String low_text = "" + w.getLow_K();
+        daily_low.setText(low_text);
+        String high_text = "" + w.getHigh_K();
+        daily_high.setText(high_text);
+        String humidity_text = "Humidity: " + w.getHumidity() + "%";
+        humidity.setText(humidity_text);
+        String wind_speed_text = "Wind Speed: " + w.getWind_Speed();
+        wind_speed.setText(wind_speed_text);
 
         System.out.println("Confirm this method is actually called");
         System.out.println("Description from weather object: " + w.getDescription());
-
-        return;
     }
 }
